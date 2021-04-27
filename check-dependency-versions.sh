82fec8b7936e33298e3f93250e2c38296af8a8a7
Niels Basjes <niels@basjes.nl>
2021-04-27 21:21:45 +0200

    Updated dependencies

--- check-dependency-versions.sh
+++ check-dependency-versions.sh
@@ -3,7 +3,7 @@
 echo "==============================="
 echo "===- CHECKING DEPENDENCIES -==="
 echo "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv"
-mvn versions:display-dependency-updates versions:display-plugin-updates | grep -E '( -> | Building )'
+mvn versions:display-dependency-updates versions:display-plugin-updates | grep -E '( -> | Building )' | grep -v 'com\.esotericsoftware:kryo .\+ 4.0.2 -> 5'
 echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
 echo "===-         DONE!         -==="
 echo "==============================="
